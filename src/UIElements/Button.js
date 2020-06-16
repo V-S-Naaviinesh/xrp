45413c684c0273c164b83a5a9c90cdb38deba180
V-S-Naaviinesh <63347826+V-S-Naaviinesh@users.noreply.github.com>
2020-06-16 10:42:31 +0800

    oversea services cancelation

--- src/UIElements/Button.js
+++ src/UIElements/Button.js
@@ -13,10 +13,10 @@ const Example = (props) => {
       <Collapse isOpen={isOpen}>
         <Card>
           <CardBody className="ShowMore">
-          A significant number of our clients are local based and few are foreign based, and we regularly act for a number of limited companies in Malaysia, Singapore , India and elsewhere. In some cases we sit on the boards of these companies. We are always prepared to act as a sounding board where required, as a kind of 'in-house counsel'.
+          A significant number of our clients are local based and few are foreign based, and we regularly act for a number of limited companies in Malaysia. In some cases we sit on the boards of these companies. We are always prepared to act as a sounding board where required.
 
 We are and have remained out of choice a relatively small firm with a compliment of 1-5 personals at any one time, together with outsourced personals. This enables us to remain flexible, efficient and cost effective.
-We are dedicated to our clients and go out of our way to provide a personal and professional service. We speak a number of languages including English, Malay , Tamil and Russian.
+We are dedicated to our clients and go out of our way to provide a personal and professional service. We speak a number of languages including English, Malay , Tamil.
 
 We are Authorised and Regulated by the Suruhanjaya Syarikat Malaysia 
 PractisingCertificate:201908001386 LisencedSecretary:LS0000223
