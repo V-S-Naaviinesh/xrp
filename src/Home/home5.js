e20c08907a23e5caffe5412f2dba6b807c534e69
V.S.Naaviinesh <v.s.naaviinesh@outlook.com>
2022-01-28 13:36:06 +0800

    onclik my name && background black && google indexnow

--- src/Home/home5.js
+++ src/Home/home5.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 
 const HomeFive = () => {
+    
+    const message ="A jack of all trades is a master of none, but oftentimes better than a master of one"
     return (
       
+        
         <div style={{color:"white"}}>
             <h6>Designed & Developed by</h6>
-            <h5 >V.S.Naaviinesh</h5>
+            <h5 style={{cursor:"pointer"}} onClick={()=>alert(message)}>V.S.Naaviinesh</h5>
         </div>
     )
 }
